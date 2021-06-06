ccb3b2125de3643bbec08f99f0c5673098bef9bb
Rafi0221 <rafal.pyzik@student.uj.edu.pl>
2021-06-06 21:25:31 +0200

    added admin layout

--- src/main/java/gui/admin/AdminController.java
+++ src/main/java/gui/admin/AdminController.java
@@ -1,4 +1,24 @@
 package gui.admin;
 
 public class AdminController {
+
+    public void dodajPracownika(){
+
+    }
+
+    public void dodajPaczkomat(){
+
+    }
+
+    public void zdezaktywujPaczkomat(){
+
+    }
+
+    public void dodajRabat(){
+
+    }
+
+    public void zmienCene(){
+
+    }
 }
